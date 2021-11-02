5d392faa42307e7e81a9be388b802919066f9d23
Sean Hall <seanhall4217@gmail.com>
2021-11-01 21:35:04 -0400

    added dependencies

---
+++ lib/widgets/metrics.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+// import 'package:metrics_widget/assets/graph.svg';
+import 'package:metrics_widget/assets/styles.dart';
+
+class Metrics extends StatefulWidget {
+  const Metrics({Key? key}) : super(key: key);
+
+  @override
+  _MetricsState createState() => _MetricsState();
+}
+
+class _MetricsState extends State<Metrics> {
+
+  final String assetName = 'assets/graph.svg';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
